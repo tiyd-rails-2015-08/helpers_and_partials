49e180b553ee56156973074d43754953722bfa2b
Joe Reiff <jcreiff@gmail.com>
2015-06-03 16:17:15 -0400

    edit total method to understand withdrawal vs deposit

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,12 @@
 class Transaction < ActiveRecord::Base
 
   def self.total
-    all.reduce(0) {|sum, t| sum + t.amount}
+    withdrawals = all.select {|t| t.transaction_type == "Withdrawal"}
+    deposits = all.select {|t| t.transaction_type == "Deposit"}
+
+    expenses = withdrawals.reduce(0) {|sum, t| sum + t.amount}
+    income = deposits.reduce(0) {|sum, t| sum + t.amount}
+
+    income - expenses
   end
 end
